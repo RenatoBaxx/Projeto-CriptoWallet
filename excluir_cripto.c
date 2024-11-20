c225c3ca6ce197c2de6a70135102041846b7aa6d
LeonardoPereira22 <lsp22112005@gmail.com>
2024-11-19 23:02:56 -0300

    att criar excluir cripto

---
+++ excluir_cripto.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char nome[50];
+    float cotacaoInicial;
+    float taxaCompra;
+    float taxaVenda;
+} Criptomoeda;
+
+void excluirCriptomoeda() {
+    char nome[50];
+    int encontrado = 0;
+    Criptomoeda cripto;
+    FILE *arquivo, *temp;
+
+    printf("\n--- Excluir Criptomoeda ---\n");
+    printf("Nome da criptomoeda a excluir: ");
+    scanf(" %[^\n]", nome);
+
+    arquivo = fopen("criptomoedas.txt", "r");
+    temp = fopen("temp.txt", "w");
+    if (arquivo == NULL || temp == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return;
+    }
+
+    while (fscanf(arquivo, "%s %f %f %f", cripto.nome, &cripto.cotacaoInicial, &cripto.taxaCompra, &cripto.taxaVenda) != EOF) {
+        if (strcmp(cripto.nome, nome) == 0) {
+            encontrado = 1;
+            printf("Criptomoeda encontrada:\n");
+            printf("Nome: %s\nCotação: %.2f\nTaxa de Compra: %.2f%%\nTaxa de Venda: %.2f%%\n", cripto.nome, cripto.cotacaoInicial, cripto.taxaCompra, cripto.taxaVenda);
+            printf("Deseja realmente excluir? (1-Sim / 0-Não): ");
+            int confirmar;
+            scanf("%d", &confirmar);
+            if (confirmar != 1) {
+                fprintf(temp, "%s %.2f %.2f %.2f\n", cripto.nome, cripto.cotacaoInicial, cripto.taxaCompra, cripto.taxaVenda);
+            }
+        } else {
+            fprintf(temp, "%s %.2f %.2f %.2f\n", cripto.nome, cripto.cotacaoInicial, cripto.taxaCompra, cripto.taxaVenda);
+        }
+    }
+    fclose(arquivo);
+    fclose(temp);
+
+    if (encontrado) {
+        remove("criptomoedas.txt");
+        rename("temp.txt", "criptomoedas.txt");
+        printf("Criptomoeda excluída com sucesso!\n");
+    } else {
+        remove("temp.txt");
+        printf("Criptomoeda não encontrada.\n");
+    }
+}
--- menu.c
+++ menu.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Declaração das funções
+
 void consultarSaldo(char *cpf);
 void consultarExtrato(char *cpf);
 void depositarReais(char *cpf);
@@ -9,52 +9,60 @@ void sacar(char *cpf);
 void comprarCriptomoedas(char *cpf);
 void venderCriptomoedas(char *cpf);
 
-// Função para exibir o menu
+
+void cadastrarCriptomoeda(); 
+void excluirCriptomoeda();   
+
+
 void exibirMenu(char *cpf) {
-  int escolha;
+    int escolha;
 
-  do {
-    printf("\n--- Menu ---\n");
-    printf("1. Consultar Saldo (Conta)\n");
-    printf("2. Consultar Extrato de Operações\n");
-    printf("3. Depositar Reais na Carteira\n");
-    printf("4. Sacar\n");
-    printf("5. Comprar Criptomoedas\n");
-    printf("6. Vender Criptomoedas\n");
-    printf("7. Atualizar Cotação\n");
-    printf("8. Sair\n");
-    printf("Escolha uma opção: ");
-    scanf("%d", &escolha);
+    do {
+        printf("\n--- Menu ---\n");
+        printf("1. Consultar Saldo (Conta)\n");
+        printf("2. Consultar Extrato de Operações\n");
+        printf("3. Depositar Reais na Carteira\n");
+        printf("4. Sacar\n");
+        printf("5. Comprar Criptomoedas\n");
+        printf("6. Vender Criptomoedas\n");
+        printf("7. Cadastrar Criptomoeda\n"); 
+        printf("8. Excluir Criptomoeda\n");   
+        printf("9. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &escolha);
 
-    // Realizando a operação correspondente com base na escolha
-    switch (escolha) {
-    case 1:
-      consultarSaldo(cpf);
-      break;
-    case 2:
-      consultarExtrato(cpf);
-      break;
-    case 3:
-      depositarReais(cpf);
-      break;
-    case 4:
-      sacar(cpf);
-      break;
-    case 5:
-      comprarCriptomoedas(cpf);
-      break;
-    case 6:
-      venderCriptomoedas(cpf);
-    break
-        case 7:
-          venderCriptomoedas(cpf);
-          break;
-    case 8:
-      printf("Saindo...\n");
-      exit(0);
-    default:
-      printf("Opção inválida! Por favor, escolha uma opção válida.\n");
-      break;
-    }
-  } while (1);
+        
+        switch (escolha) {
+            case 1:
+                consultarSaldo(cpf);
+                break;
+            case 2:
+                consultarExtrato(cpf);
+                break;
+            case 3:
+                depositarReais(cpf);
+                break;
+            case 4:
+                sacar(cpf);
+                break;
+            case 5:
+                comprarCriptomoedas(cpf);
+                break;
+            case 6:
+                venderCriptomoedas(cpf);
+                break;
+            case 7:
+                cadastrarCriptomoeda(); 
+                break;
+            case 8:
+                excluirCriptomoeda();  
+                break;
+            case 9:
+                printf("Saindo...\n");
+                exit(0);
+            default:
+                printf("Opção inválida! Por favor, escolha uma opção válida.\n");
+                break;
+        }
+    } while (1);
 }
